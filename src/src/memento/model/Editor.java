32390cebc5653582d7143eb77e6c762562d4ab0a
Styfjion <17717092026@163.com>
2022-04-10 19:52:53 +0800

    备忘录

---
+++ src/src/memento/model/Editor.java
@@ -0,0 +1,68 @@
+package memento.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Editor {
+    private final Doc doc;
+    private final List<History> historyRecords;
+    private int historyPosition = -1;
+
+    public Editor(Doc doc) {
+        System.out.println("<<<打开文档" + doc.getTitle());
+        this.doc = doc;
+        historyRecords = new ArrayList<>();
+        backup();
+        show();
+    }
+
+    public void append(String txt) {
+        System.out.println("<<<插入操作");
+        doc.setBody(doc.getBody() + txt);
+        backup();
+        show();
+    }
+
+    public void save() {
+        System.out.println("<<<存盘操作");
+    }
+
+    public void delete() {
+        System.out.println("<<<删除操作");
+        doc.setBody("");
+        backup();
+        show();
+    }
+
+    private void backup() {
+        historyRecords.add(doc.createHistory());
+        historyPosition++;
+    }
+
+    private void show() {
+        System.out.println(doc.getBody());
+        System.out.println("<<<文档结束");
+    }
+
+    public void undo() {
+        System.out.println("<<<撤销操作");
+        if (historyPosition == 0) {
+            return;
+        }
+        historyPosition--;
+        History history = historyRecords.get(historyPosition);
+        doc.restoreHistory(history);
+        show();
+    }
+
+    public void redo() {
+        System.out.println("<<<回退撤销操作");
+        if (historyPosition >= historyRecords.size()) {
+            return;
+        }
+        historyPosition++;
+        History history = historyRecords.get(historyPosition);
+        doc.restoreHistory(history);
+        show();
+    }
+}
---
+++ src/src/memento/model/History.java
@@ -0,0 +1,17 @@
+package memento.model;
+
+public class History {
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public History(String body) {
+        this.body = body;
+    }
+}
